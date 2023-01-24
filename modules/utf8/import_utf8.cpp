8dc301a3429b6c1c4fc6cdcfa6f37988db4eb1a4
janbar <jlbarriere68@gmail.com>
2023-01-25 00:06:53 +0100

    adding new module to handle type utf8 (ucs format 8)

---
+++ modules/utf8/import_utf8.cpp
@@ -0,0 +1,281 @@
+/*
+ *      Copyright (C) 2022 Jean-Luc Barriere
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "import_utf8.h"
+#include "utf8helper.h"
+
+/*
+ * Create the module UTF8Import
+ */
+IMPORTCREATOR(UTF8Import)
+
+namespace bloc
+{
+namespace import
+{
+namespace utf8
+{
+
+/*
+ * The module interface
+ */
+
+/**********************************************************************/
+/*  Constructors                                                      */
+/**********************************************************************/
+static IMPORT_TYPE ctor_0_args[]  = {
+  { "C", 0 }, // utf8
+};
+
+static IMPORT_TYPE ctor_1_args[]  = {
+  { "L", 0 }, // string
+};
+
+static IMPORT_CTOR ctors[] =
+{
+  { 0,      1,  ctor_0_args },  /* copy ctor */
+  { 1,      1,  ctor_1_args },  /* new utf8( string ) */
+};
+
+enum Method
+{
+  Empty = 0, Size, Rawsize, Reserve, Clear,
+  Append, AppendL, ConcatC,
+  Tostring, At,
+  Remove, Insert, InsertC, Substr1, Substr2,
+  Toupper, Tolower, Normaliz, Capital, Translit,
+};
+
+/**********************************************************************/
+/*  Method arguments                                                  */
+/*  mode:         type: decl,ndim                                     */
+/**********************************************************************/
+static IMPORT_ARG int_args[]  = {
+  { IMPORT_IN,    { "I", 0 } }, // integer
+};
+
+static IMPORT_ARG string_args[]  = {
+  { IMPORT_IN,    { "L", 0 } }, // string
+};
+
+static IMPORT_ARG utf8_args[]  = {
+  { IMPORT_IN,    { "C", 0 } }, // utf8
+};
+
+static IMPORT_ARG remove_args[]  = {
+  { IMPORT_IN,    { "I", 0 } }, // pos
+  { IMPORT_IN,    { "I", 0 } }, // size
+};
+
+static IMPORT_ARG insert_args[]  = {
+  { IMPORT_IN,    { "I", 0 } }, // pos
+  { IMPORT_IN,    { "I", 0 } }, // codepoint
+};
+
+static IMPORT_ARG insertC_args[]  = {
+  { IMPORT_IN,    { "I", 0 } }, // integer
+  { IMPORT_IN,    { "C", 0 } }, // utf8
+};
+
+static IMPORT_ARG substr_args[]  = {
+  { IMPORT_IN,    { "I", 0 } }, // pos
+  { IMPORT_IN,    { "I", 0 } }, // size
+};
+
+/**********************************************************************/
+/*  Methods list                                                      */
+/*  id:       name:         ret: decl,ndim  args_count,args:          */
+/**********************************************************************/
+static IMPORT_METHOD methods[] =
+{
+  { Empty,    "empty",      { "B", 0 },     0, nullptr, },
+
+  { Size,     "count",      { "I", 0 },     0, nullptr, },
+
+  { Rawsize,  "rawsize",    { "I", 0 },     0, nullptr, },
+
+  { Reserve,  "reserve",    { "B", 0 },     1, int_args, },
+
+  { Clear,    "clear",      { "B", 0 },     0, nullptr, },
+
+  { Append,   "append",     { "C", 0 },     1, int_args, },
+
+  { AppendL,  "append",     { "C", 0 },     1, string_args, },
+
+  { ConcatC,  "concat",     { "C", 0 },     1, utf8_args, },
+
+  { Tostring, "string",     { "L", 0 },     0, nullptr, },
+
+  { At,       "at",         { "I", 0 },     1, int_args, },
+
+  { Remove,   "remove",     { "B", 0 },     2, remove_args, },
+
+  { Insert,   "insert",     { "B", 0 },     2, insert_args, },
+
+  { InsertC,  "insert",     { "I", 0 },     2, insertC_args, },
+
+  { Substr1,  "substr",     { "L", 0 },     1, int_args, },
+
+  { Substr2,  "substr",     { "L", 0 },     2, substr_args, },
+
+  { Toupper,  "toupper",    { "C", 0 },     0, nullptr, },
+
+  { Tolower,  "tolower",    { "C", 0 },     0, nullptr, },
+
+  { Normaliz, "normalize",  { "C", 0 },     0, nullptr, },
+
+  { Capital,  "capitalize", { "C", 0 },     0, nullptr, },
+
+  { Translit, "translit",   { "C", 0 },     0, nullptr, },
+};
+
+} /* namespace utf8 */
+
+void UTF8Import::declareInterface(IMPORT_INTERFACE * interface)
+{
+  interface->name = "utf8";
+  interface->method_count = sizeof(utf8::methods) / sizeof(IMPORT_METHOD);
+  interface->methods = utf8::methods;
+  interface->ctors_count = sizeof(utf8::ctors) / sizeof(IMPORT_CTOR);
+  interface->ctors = utf8::ctors;
+}
+
+void * UTF8Import::createObject(int ctor_id, bloc::Context& ctx, const std::vector<bloc::Expression*>& args)
+{
+  switch (ctor_id)
+  {
+  case 0: /* copy ctor */
+  {
+    Complex c0 = args[0]->complex(ctx);
+    utf8helper::UTF8String * u = static_cast<utf8helper::UTF8String*>(c0.instance());
+    return new utf8helper::UTF8String(*u);
+  }
+
+  case 1: /* utf8( string ) */
+    return new utf8helper::UTF8String(args[0]->literal(ctx));
+
+  default: /* default ctor */
+    return new utf8helper::UTF8String();
+  }
+  return nullptr;
+}
+
+void UTF8Import::destroyObject(void * object)
+{
+  utf8helper::UTF8String * u = static_cast<utf8helper::UTF8String*>(object);
+  delete u;
+}
+
+bloc::Expression * UTF8Import::executeMethod(
+          bloc::Complex object_this,
+          int method_id,
+          bloc::Context& ctx,
+          const std::vector<bloc::Expression*>& args
+          )
+{
+  utf8helper::UTF8String * u = static_cast<utf8helper::UTF8String*>(object_this.instance());
+  switch (method_id)
+  {
+  case utf8::Empty:
+    return new BooleanExpression(u->Empty());
+
+  case utf8::Size:
+    return new IntegerExpression((int64_t) u->Size());
+
+  case utf8::Rawsize:
+    return new IntegerExpression((int64_t) u->RawSize());
+
+  case utf8::Reserve:
+    u->Reserve(args[0]->integer(ctx));
+    return new BooleanExpression(true);
+
+  case utf8::Clear:
+    u->Clear();
+    return new BooleanExpression(true);
+
+  case utf8::Append:
+    u->Append(args[0]->integer(ctx));
+    return new ComplexExpression(object_this);
+
+  case utf8::AppendL:
+  {
+    std::string& str = args[0]->literal(ctx);
+    for (auto& c : str)
+      u->WriteByte(c);
+    return new ComplexExpression(object_this);
+  }
+
+  case utf8::ConcatC:
+  {
+    Complex c0 = args[0]->complex(ctx);
+    utf8helper::UTF8String * u0 = static_cast<utf8helper::UTF8String*>(c0.instance());
+    u->Append(u0->Data());
+    return new ComplexExpression(object_this);
+  }
+
+  case utf8::Tostring:
+    return new LiteralExpression(u->ToStdString());
+
+  case utf8::At:
+    return new IntegerExpression(u->operator[](args[0]->integer(ctx)));
+
+  case utf8::Remove:
+    return new BooleanExpression(u->Remove((size_t)args[0]->integer(ctx), (size_t)args[1]->integer(ctx)));
+
+  case utf8::Insert:
+    return new BooleanExpression(u->Insert((size_t)args[0]->integer(ctx), (utf8helper::codepoint)args[1]->integer(ctx)));
+
+  case utf8::InsertC:
+  {
+    Complex c0 = args[1]->complex(ctx);
+    utf8helper::UTF8String * u0 = static_cast<utf8helper::UTF8String*>(c0.instance());
+    return new IntegerExpression((int64_t)u->Insert((size_t)args[0]->integer(ctx), u0->Data()));
+  }
+
+  case utf8::Substr1:
+    return new LiteralExpression(u->Substr((size_t)args[0]->integer(ctx)));
+
+  case utf8::Substr2:
+    return new LiteralExpression(u->Substr((size_t)args[0]->integer(ctx), (size_t)args[1]->integer(ctx)));
+
+  case utf8::Tolower:
+    u->Transform(utf8helper::TransformLower);
+    return new ComplexExpression(object_this);
+
+  case utf8::Toupper:
+    u->Transform(utf8helper::TransformUpper);
+    return new ComplexExpression(object_this);
+
+  case utf8::Normaliz:
+    u->Transform(utf8helper::TransformNormalize);
+    return new ComplexExpression(object_this);
+
+  case utf8::Capital:
+    u->Transform(utf8helper::TransformCapitalize);
+    return new ComplexExpression(object_this);
+
+  case utf8::Translit:
+    u->Transform(utf8helper::TransformTransliterate);
+    return new ComplexExpression(object_this);
+
+  }
+  return nullptr;
+}
+
+} /* namespace import */
+} /* namespace bloc */
---
+++ modules/utf8/utf8helper.cpp
@@ -0,0 +1,501 @@
+/*
+  Copyright (C) 2021  Jean-Luc Barriere
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include "utf8helper.h"
+
+#include <cstring>
+
+namespace utf8helper
+{
+
+std::string UTF8ToUpper(const std::string& text) {
+  return UTF8String(text, TransformUpper).ToStdString();
+}
+
+std::string UTF8ToLower(const std::string& text) {
+  return UTF8String(text, TransformLower).ToStdString();
+}
+
+std::string UTF8Normalize(const std::string& text) {
+  return UTF8String(text, TransformNormalize).ToStdString();
+}
+
+std::string UTF8Capitalize(const std::string& text) {
+  return UTF8String(text, TransformCapitalize).ToStdString();
+}
+
+std::string UTF8Transliterate(const std::string& text) {
+  return UTF8String(text, TransformTransliterate).ToStdString();
+}
+
+
+codepoint TransformNop(const character* ch, int context) {
+  (void)context;
+  return ch->code;
+}
+
+codepoint TransformUpper(const character* ch, [[maybe_unused]] int context) {
+  return ch->upper;
+}
+
+codepoint TransformLower(const character* ch, [[maybe_unused]] int context) {
+  return ch->lower;
+}
+
+codepoint TransformNormalize(const character* ch, int context) {
+  if ((IsSpace | IsBreaker) & ch->category) {
+    if ((IsSpace | IsBreaker) & context) {
+      return NullCodepoint;
+    }
+    return 0x20;
+  }
+  if (ch->category & IsControl) {
+    return NullCodepoint;
+  }
+  return ch->lower;
+}
+
+codepoint TransformCapitalize(const character* ch, int context) {
+  if (context & (IsSpace | IsBreaker | IsControl)) {
+    return ch->upper;
+  }
+  return ch->lower;
+}
+
+codepoint TransformTransliterate(const character* ch, int context) {
+  codepoint c = NullCodepoint;
+  if (IsSpace & context & ch->category) {
+    return c;
+  }
+  for (const char* b = ch->translate; *b; ++b) {
+    c <<= 8;
+    c |= static_cast<byte>(*b);
+  }
+  return c;
+}
+
+
+static inline char* _u_string(char* buf, codepoint u) {
+  if (u < 0x100) {
+    *(buf++) = (0xff & u);
+  } else if (u < 0x10000) {
+    *(buf++) = (0xff & (u >> 8));
+    *(buf++) = (0xff & u);
+  } else if (u < 0x1000000) {
+    *(buf++) = (0xff & (u >> 16));
+    *(buf++) = (0xff & (u >> 8));
+    *(buf++) = (0xff & u);
+  } else {
+    *(buf++) = (0xff & (u >> 24));
+    *(buf++) = (0xff & (u >> 16));
+    *(buf++) = (0xff & (u >> 8));
+    *(buf++) = (0xff & u);
+  }
+  *buf = 0;
+  return buf;
+}
+
+static inline int _u_size(codepoint u) {
+  if (u < 0x100) {
+    return 1;
+  } else if (u < 0x10000) {
+    return 2;
+  } else if (u < 0x1000000) {
+    return 3;
+  }
+  return 4;
+}
+
+UTF8String::UTF8String()
+: parser(utf8helper::TransformNop), rawSize(0) { }
+
+UTF8String::UTF8String(const std::string& text)
+: UTF8String() {
+  Transform(text, parser.func);
+}
+
+UTF8String::UTF8String(utf8helper::Transform func)
+: parser(func), rawSize(0) { }
+
+UTF8String::UTF8String(const std::string& text, utf8helper::Transform func)
+: parser(func), rawSize(0) {
+  Transform(text, func);
+}
+
+void UTF8String::Clear() {
+  parser.Reset();
+  store.clear();
+  rawSize = 0;
+}
+
+void UTF8String::WriteByte(char cc) {
+  switch (parser.run(&parser, static_cast<byte>(cc))) {
+  case Parser::Done:
+    store.push_back(parser.u);
+    rawSize += parser.u_size;
+    break;
+  case Parser::Continue:
+    break;
+  case Parser::Error:
+    break;
+  }
+}
+
+bool UTF8String::Remove(size_t pos, size_t n) {
+  if (pos < Size()) {
+    if (n > (store.size() - pos)) {
+      n = store.size() - pos;
+    }
+    size_t bc = 0;
+    storage_type::const_iterator beg = store.cbegin() + pos;
+    storage_type::const_iterator end = beg + n;
+    for (storage_type::const_iterator it = beg; it != end; ++it) {
+      bc += _u_size(*it);
+    }
+    store.erase(beg, end);
+    rawSize -= bc;
+    return true;
+  }
+  return false;
+}
+
+bool UTF8String::Insert(size_t pos, codepoint u, utf8helper::Transform func) {
+  if (pos <= Size()) {
+    Parser p(func);
+    char buf[5];
+    _u_string(buf, u);
+    for (char* b = buf; *b; ++b) {
+      switch (p.run(&p, static_cast<byte>(*b))) {
+      case Parser::Done:
+        store.insert(store.begin() + pos, p.u);
+        rawSize += p.u_size;
+        return true;
+      case Parser::Continue:
+        break;
+      case Parser::Error:
+        return false;
+      }
+    }
+  }
+  return false;
+}
+
+size_t UTF8String::Insert(size_t pos, const storage_type& data, utf8helper::Transform func) {
+  size_t n = 0;
+  if (pos <= Size()) {
+    storage_type _data(data);
+    for (const codepoint& u : _data) {
+      if (Insert(pos + n, u, func)) {
+        ++n;
+      }
+    }
+  }
+  return n;
+}
+
+UTF8String &UTF8String::Append(codepoint u, utf8helper::Transform func) {
+  Parser p(func);
+  char buf[5];
+  _u_string(buf, u);
+  for (char* b = buf; *b; ++b) {
+    switch (p.run(&p, static_cast<byte>(*b))) {
+    case Parser::Done:
+      store.push_back(p.u);
+      rawSize += p.u_size;
+      return *this;
+    case Parser::Continue:
+      break;
+    case Parser::Error:
+      return *this;
+    }
+  }
+  return *this;
+}
+
+UTF8String &UTF8String::Append(const UTF8String::storage_type &data, utf8helper::Transform func) {
+  storage_type _data(data);
+  for (const codepoint& u : _data) {
+    Append(u, func);
+  }
+  return *this;
+}
+
+UTF8String& UTF8String::Transform(utf8helper::Transform func) {
+  storage_type tmp(store);
+  Clear();
+  parser.func = func;
+  for (const codepoint& u : tmp) {
+    char buf[5];
+    _u_string(buf, u);
+    for (char* b = buf; *b; ++b) {
+      WriteByte(*b);
+    }
+  }
+  return *this;
+}
+
+UTF8String& UTF8String::Transform(const std::string& text, utf8helper::Transform func) {
+  Clear();
+  Reserve(text.size());
+  parser.func = func;
+  for (const char& cc : text) {
+    WriteByte(cc);
+  }
+  return *this;
+}
+
+std::string UTF8String::ToStdString() const {
+  std::string tmp(rawSize, '$');
+  char* b = &tmp[0];
+  for (const codepoint& u : store) {
+    b = _u_string(b, u);
+  }
+  tmp.resize(b - &tmp[0]);
+  return tmp;
+}
+
+std::string UTF8String::Substr(size_t pos, size_t n) const {
+  if (pos < store.size()) {
+    if (n > (store.size() - pos)) {
+      n = store.size() - pos;
+    }
+    std::string tmp(sizeof(codepoint) * n, '$');
+    storage_type::const_iterator it = store.cbegin() + pos;
+    char* b = &tmp[0];
+    for (; it != store.cend() && n > 0; ++it, --n) {
+      b = _u_string(b, *it);
+    }
+    tmp.resize(b - &tmp[0]);
+    return tmp;
+  }
+  return "";
+}
+
+
+/**
+ * 1 byte:
+ * RFC 3629:#4: Valid UTF-8 matches the following syntax
+ * 00-7F
+ *
+ * 2 bytes:
+ * RFC 3629:#4: Valid UTF-8 matches the following syntax
+ * C2-DF 80-BF
+ *
+ * 3 bytes:
+ * RFC 3629:#4: Valid UTF-8 matches the following syntax
+ * E0    A0-BF 80-BF
+ * E1-EC 80-BF 80-BF
+ * ED    80-9F 80-BF
+ * EE-EF 80-BF 80-BF
+ * RFC 3629:#6: [EF,BB,BF] is BOM on start, else ZERO WIDTH NO-BREAK SPACE
+ *
+ * 4 bytes:
+ * RFC 3629:#4: Valid UTF-8 matches the following syntax
+ * F0    90-BF 80-BF 80-BF
+ * F1-F3 80-BF 80-BF 80-BF
+ * F4    80-8F 80-BF 80-BF
+ */
+static Parser::Exit _p1_u2(Parser* p, byte bb);
+static Parser::Exit _p1_u3(Parser* p, byte bb);
+static Parser::Exit _p2_u3(Parser* p, byte bb);
+static Parser::Exit _p1_u4(Parser* p, byte bb);
+static Parser::Exit _p2_u4(Parser* p, byte bb);
+static Parser::Exit _p3_u4(Parser* p, byte bb);
+
+static Parser::Exit _p0(Parser* p, byte bb) {
+  if (bb < 0x80) {
+    const character* c = &charmap_us7ascii[bb];
+    codepoint u = p->func(c, p->context);
+    if (u == NullCodepoint) {
+      return Parser::Continue;
+    }
+    p->u = u;
+    p->context = c->category;
+    p->u_size = _u_size(p->u);
+    return Parser::Done;
+  }
+  else if (bb < 0xc2) {
+    // invalid codepoint
+    return Parser::Error;
+  }
+  else if (bb < 0xe0) {
+    p->b[0] = bb;
+    p->run = _p1_u2;
+    return Parser::Continue;
+  }
+  else if (bb < 0xf0) {
+    p->b[0] = bb;
+    p->run = _p1_u3;
+    return Parser::Continue;
+  }
+  else if (bb < 0xf5) {
+    p->b[0] = bb;
+    p->run = _p1_u4;
+    return Parser::Continue;
+  }
+  else {
+    // invalid codepoint
+    return Parser::Error;
+  }
+}
+
+static Parser::Exit _p1_u2(Parser* p, byte bb) {
+  p->run = _p0;
+  if (bb > 0x7f && bb < 0xc0) {
+    const character* pg = pagemap_16[p->b[0] - 0xc0];
+    if (pg) {
+      const character* c = &pg[bb - 0x80];
+      codepoint u = p->func(c, p->context);
+      if (u == NullCodepoint)
+        return Parser::Continue;
+      p->u = u;
+      p->context = c->category;
+    } else {
+      p->u = (p->b[0] << 8) | bb;
+      p->context = None;
+    }
+    p->u_size = _u_size(p->u);
+    return Parser::Done;
+  }
+  // invalid codepoint: restart
+  return _p0(p, bb);
+}
+
+static Parser::Exit _p1_u3(Parser* p, byte bb) {
+  if ( (p->b[0] == 0xe0 && bb > 0x9f && bb < 0xc0) ||
+       (p->b[0] > 0xe0 && p->b[0] < 0xed && bb > 0x7f && bb < 0xc0) ||
+       (p->b[0] == 0xed && bb > 0x7f && bb < 0xa0) ||
+       (p->b[0] > 0xed && bb > 0x7f && bb < 0xc0) ) {
+    p->b[1] = bb;
+    p->run = _p2_u3;
+    return Parser::Continue;
+  }
+  // invalid codepoint: restart
+  p->run = _p0;
+  return _p0(p, bb);
+}
+
+static Parser::Exit _p2_u3(Parser* p, byte bb) {
+  p->run = _p0;
+  if (bb > 0x7f && bb < 0xc0) {
+    const character* pg = nullptr;
+    switch (p->b[0]) {
+    case 0xe1:
+      pg = pagemap_24_e1[p->b[1] - 0x80];
+      break;
+    case 0xe2:
+      pg = pagemap_24_e2[p->b[1] - 0x80];
+      break;
+    default:
+      // no code page defined
+      break;
+    }
+    if (pg) {
+      const character* c = &pg[bb - 0x80];
+      codepoint u = p->func(c, p->context);
+      if (u == NullCodepoint)
+        return Parser::Continue;
+      p->u = u;
+      p->context = c->category;
+    } else {
+      p->u = (p->b[0] << 16) | (p->b[1] << 8) | bb;
+      p->context = None;
+    }
+    p->u_size = _u_size(p->u);
+    return Parser::Done;
+  }
+  // invalid codepoint: restart
+  return _p0(p, bb);
+}
+
+static Parser::Exit _p1_u4(Parser* p, byte bb) {
+  if ( (p->b[0] == 0xf0 && bb > 0x8f && bb < 0xc0) ||
+       (p->b[0] > 0xf0 && p->b[0] < 0xf4 && bb > 0x7f && bb < 0xc0) ||
+       (p->b[0] == 0xf4 && bb > 0x7f && bb < 0x90) ) {
+    p->b[1] = bb;
+    p->run = _p2_u4;
+    return Parser::Continue;
+  }
+  // invalid codepoint: restart
+  p->run = _p0;
+  return _p0(p, bb);
+}
+
+static Parser::Exit _p2_u4(Parser* p, byte bb) {
+  if (bb > 0x7f && bb < 0xc0) {
+    p->b[2] = bb;
+    p->run = _p3_u4;
+    return Parser::Continue;
+  }
+  // invalid codepoint: restart
+  p->run = _p0;
+  return _p0(p, bb);
+}
+
+static Parser::Exit _p3_u4(Parser* p, byte bb) {
+  p->run = _p0;
+  if (bb > 0x7f && bb < 0xc0) {
+    const character* pg = nullptr;
+    switch (p->b[0]) {
+    case 0xf0:
+      switch (p->b[1]) {
+      case 0x90:
+        pg = pagemap_32_f0_90[p->b[2] - 0x80];
+        break;
+      case 0x9e:
+        pg = pagemap_32_f0_9e[p->b[2] - 0x80];
+        break;
+      default:
+        // no code page defined
+        break;
+      }
+      break;
+    default:
+      // no code page defined
+      break;
+    }
+    if (pg) {
+      const character* c = &pg[bb - 0x80];
+      codepoint u = p->func(c, p->context);
+      if (u == NullCodepoint)
+        return Parser::Continue;
+      p->u = u;
+      p->context = c->category;
+    } else {
+      p->u = (p->b[0] << 24) | (p->b[1] << 16) | (p->b[2] << 8) | bb;
+      p->context = None;
+    }
+    p->u_size = _u_size(p->u);
+    return Parser::Done;
+  }
+  // invalid codepoint: restart
+  return _p0(p, bb);
+}
+
+Parser::Parser(Transform func) : func(func) {
+  Reset();
+}
+
+void Parser::Reset() {
+  run = _p0;
+  context = IsSpace|IsBreaker;
+  u_size = 0;
+  u = NullCodepoint;
+}
+
+}
