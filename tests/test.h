81f32a0b3ee29c3ca3bdbd827426d594f84c58fd
janbar <jlbarriere68@gmail.com>
2023-01-28 14:05:48 +0100

    use helper class StringReader for testing

--- tests/test.h
+++ tests/test.h
@@ -6,46 +6,18 @@
 
 #include <blocc/context.h>
 #include <blocc/parser.h>
-
-#include <string>
-
-class IBuffer
-{
-  std::string _text;
-  size_t _pos = 0;
-
-public:
-  virtual ~IBuffer() = default;
-  IBuffer() { }
-  IBuffer& reset(const std::string& text) { _text.assign(text); _pos = 0; return *this; }
-  IBuffer& append(const std::string& text) { _text.append(text); return *this; }
-
-  int read(char * buf, int max_size)
-  {
-    size_t r = (max_size  > _text.size() - _pos ? _text.size() - _pos : max_size);
-    ::memcpy(buf, &_text[_pos], r);
-    _pos += r;
-    return (int) r;
-  }
-
-  static void read_input(void * handle, char * buf, int * len, int max_size)
-  {
-    IBuffer * ib = static_cast<IBuffer*>(handle);
-    *len = ib->read(buf, max_size);
-  }
-};
-
+#include <blocc/string_reader.h>
 
 class TestingContext : public bloc::Context
 {
-  IBuffer input;
+  bloc::StringReader input;
   bloc::Parser * parser;
 
 public:
   virtual ~TestingContext () { delete parser; }
   TestingContext () : Context ()
   {
-    parser = bloc::Parser::createInteractiveParser (*this, &input, &IBuffer::read_input);
+    parser = bloc::Parser::createInteractiveParser (*this, &input, &bloc::StringReader::read_input);
   }
 
   void reset (const std::string &text)
@@ -68,9 +40,8 @@ public:
   bloc::Executable * parse ()
   {
     parser->clear();
-    return bloc::Parser::parse (*this, &input, &IBuffer::read_input);
+    return bloc::Parser::parse (*this, &input, &bloc::StringReader::read_input);
   }
 };
 
 #endif
-
