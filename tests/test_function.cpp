9beae76b4691737249e85ac8fed2c0dd0eb5a3af
janbar <jlbarriere68@gmail.com>
2023-04-11 23:33:38 +0200

    add tests of function

--- tests/test_function.cpp
+++ tests/test_function.cpp
@@ -11,153 +11,92 @@ TestingContext ctx;
 
 using namespace bloc;
 
-TEST_CASE("raise user")
+TEST_CASE("create and replace function")
 {
   ctx.purge();
   Executable * e;
   ctx.reset(
+          "function func(r) returns decimal is\n"
           "begin\n"
-          "  raise myerror;\n"
-          "exception\n"
-          "when divide_by_zero then return 1;"
-          "when myerror then return 2;"
-          "when others then return 3;"
-          "end; return 0;"
+          "  fn2 = 12.0 , fn1 = 6.0;\n"
+          "  for i in 2 to r loop\n"
+          "    c = fn1 + fn2;\n"
+          "    fn2 = fn1;\n"
+          "    fn1 = c;\n"
+          "  end loop;\n"
+          "  return fn1 / fn2;\n"
+          "end;\n"
   );
   e = ctx.parse();
   REQUIRE( e->run() == 0 );
   delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r->integer(ctx) == 2 );
-  delete r;
-}
 
-TEST_CASE("raise divide_by_zero")
-{
-  ctx.purge();
-  Executable * e;
+  ctx.reset("a = round(func(100),6);");
+  e = ctx.parse();
+  REQUIRE( e->run() == 0 );
+  delete e;
+  REQUIRE( ctx.loadVariable("A")->numeric(ctx) == 1.618034 );
+
   ctx.reset(
+          "function func(s) returns string is\n"
           "begin\n"
-          "  raise divide_by_zero;\n"
-          "exception\n"
-          "when divide_by_zero then return 4;"
-          "when myerror then return 5;"
-          "when others then return 6;"
-          "end; return 0;"
+          "  return upper(s);\n"
+          "end;\n"
   );
   e = ctx.parse();
   REQUIRE( e->run() == 0 );
   delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r->integer(ctx) == 4 );
-  delete r;
+
+  ctx.reset("a = func(\"hello world!\");");
+  e = ctx.parse();
+  REQUIRE( e->run() == 0 );
+  delete e;
+  REQUIRE( ctx.loadVariable("A")->literal(ctx) == "HELLO WORLD!" );
 }
 
-TEST_CASE("throw divide_by_zero")
+TEST_CASE("function returns table")
 {
   ctx.purge();
   Executable * e;
   ctx.reset(
+          "function func(t) returns table is\n"
           "begin\n"
-          "  do 1/0;\n"
-          "exception\n"
-          "when myerror then return 7;"
-          "when divide_by_zero then return 8;"
-          "when others then return 9;"
-          "end; return 0;"
+          "  for i in 0 to t.count()-1 loop t.put(i, 1); end loop;\n"
+          "  return t;\n"
+          "end;\n"
   );
   e = ctx.parse();
   REQUIRE( e->run() == 0 );
   delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r->integer(ctx) == 8 );
-  delete r;
+
+  ctx.reset(
+          "a = func(tab(100,0)), b = 0;\n"
+          "for i in 0 to a.count()-1 loop b = b + a.at(i); end loop;");
+  e = ctx.parse();
+  REQUIRE( e->run() == 0 );
+  delete e;
+  REQUIRE( ctx.loadVariable("B")->integer(ctx) == 100 );
 }
 
-TEST_CASE("raise out_of_range")
+TEST_CASE("function returns tuple")
 {
   ctx.purge();
   Executable * e;
   ctx.reset(
+          "function func(t) returns tuple is\n"
           "begin\n"
-          "  raise out_of_range;\n"
-          "exception\n"
-          "when myerror then return 10;"
-          "when out_of_range then return 11;"
-          "when others then return 12;"
-          "end; return 0;"
+          "  t.set@1(upper(t@1));\n"
+          "  return t;\n"
+          "end;\n"
   );
   e = ctx.parse();
   REQUIRE( e->run() == 0 );
   delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r->integer(ctx) == 11 );
-  delete r;
-}
 
-TEST_CASE("raise others")
-{
-  ctx.purge();
-  Executable * e;
   ctx.reset(
-          "begin\n"
-          "  raise out_of_range;\n"
-          "exception\n"
-          "when divide_by_zero then return 13;"
-          "when myerror then return 14;"
-          "when others then return 15;"
-          "end; return 0;"
-  );
+          "a = func(tup(\"hello\", 0.0, true)), b = a@1;");
   e = ctx.parse();
   REQUIRE( e->run() == 0 );
   delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r->integer(ctx) == 15 );
-  delete r;
-}
-
-TEST_CASE("raise not catched")
-{
-  ctx.purge();
-  Executable * e;
-  ctx.reset(
-          "begin\n"
-          "  raise out_of_range;\n"
-          "exception\n"
-          "when divide_by_zero then return 16;"
-          "when myerror then return 17;"
-          "end; return 0;"
-  );
-  e = ctx.parse();
-  REQUIRE_THROWS_AS( e->run(), bloc::RuntimeError);
-  delete e;
-}
-
-TEST_CASE("unstacking on exception")
-{
-  ctx.purge();
-  Executable * e;
-  Symbol& a1 = ctx.registerSymbol("$1", Type::INTEGER);
-  ctx.storeVariable(a1, IntegerExpression(100));
-  ctx.reset(
-          "brk=$1*2, cnt=0;\n"
-          "for i in 1 to $1 loop\n"
-          "  brk=brk-1; if brk <= 0 then return 0; end if;\n"
-          "  begin\n"
-          "    for j in 2 to i/2 asc loop\n"
-          "      if i%j == 0 then raise notprim; end if;\n"
-          "    end loop;\n"
-          "    cnt=cnt+1;\n"
-          "  exception when notprim then nop;\n"
-          "  end;\n"
-          "end loop;\n"
-          "return cnt;"
-  );
-  e = ctx.parse();
-  REQUIRE( e->run() == 0 );
-  delete e;
-  Expression * r = ctx.dropReturned();
-  REQUIRE( r != nullptr );
-  REQUIRE( r->integer(ctx) == 26 );
-  delete r;
+  REQUIRE( ctx.loadVariable("B")->literal(ctx) == "HELLO" );
 }
