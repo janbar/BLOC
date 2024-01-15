394c2593dd9c541ffc855458c1074648b15d5f41
janbar <jlbarriere68@gmail.com>
2024-01-16 00:32:39 +0100

    handle null for add, sub, mul, div

--- blocc/operator/op_add.cpp
+++ blocc/operator/op_add.cpp
@@ -50,80 +50,110 @@ const Type& OpADDExpression::type(Context& ctx) const
 }
 
 #define LVAL1(V,A) (\
- !A.lvalue() ? (A = std::move(V)) : ctx.allocate(V.clone()) \
+ !A.lvalue() ? (A = std::move(V)) : ctx.allocate(std::move(V)) \
 )
 
 #define LVAL2(V,A,B) (\
  !A.lvalue() ? (A = std::move(V)) : \
- !B.lvalue() ? (B = std::move(V)) : ctx.allocate(V.clone()) \
+ !B.lvalue() ? (B = std::move(V)) : ctx.allocate(std::move(V)) \
 )
 
 Value& OpADDExpression::value(Context& ctx) const
 {
   Value& a1 = arg1->value(ctx);
+  Value& a2 = arg2->value(ctx);
 
-  switch (a1.type().major())
+  if (a1.type().level() == 0 && a2.type().level() == 0)
   {
-  case Type::NO_TYPE:
-  {
-    Value& a2 = arg2->value(ctx);
-    /* concat literal */
-    if (a2.type() == Type::LITERAL)
-      return a2;
-    return a1;
-  }
-  case Type::INTEGER:
-  {
-    if (a1.isNull())
-      return a1;
-    Value& a2 = arg2->value(ctx);
-    if (a2.isNull())
-      return a2;
-    if (a2.type() == Type::NUMERIC)
+    switch (a1.type().major())
     {
-      Value val(Numeric(*a1.integer() + *a2.numeric()));
-      return LVAL2(val, a1, a2);
+    case Type::NO_TYPE:
+      switch (a2.type().major())
+      {
+      case Type::NO_TYPE:
+        return LVAL2(Value(Value::type_numeric), a1, a2);
+      case Type::INTEGER:
+      case Type::NUMERIC:
+        return LVAL2(Value(a2.type()), a1, a2);
+      case Type::LITERAL:
+        return a2; /* null + literal */
+      default:
+        break;
+      }
+      break;
+    case Type::INTEGER:
+      switch (a2.type().major())
+      {
+      case Type::NO_TYPE:
+        return LVAL2(Value(Value::type_integer), a1, a2);
+      case Type::NUMERIC:
+      {
+        if (a2.isNull() || a1.isNull())
+          return LVAL2(Value(Value::type_numeric), a1, a2);
+        Value val(Numeric(*a1.integer() + *a2.numeric()));
+        return LVAL2(val, a1, a2);
+      }
+      case Type::INTEGER:
+      {
+        if (a2.isNull() || a1.isNull())
+          return LVAL2(Value(Value::type_integer), a1, a2);
+        Value val(Integer(*a1.integer() + *a2.integer()));
+        return LVAL2(val, a1, a2);
+      }
+      default:
+        break;
+      }
+      break;
+    case Type::NUMERIC:
+      switch (a2.type().major())
+      {
+      case Type::NO_TYPE:
+        return LVAL2(Value(Value::type_numeric), a1, a2);
+      case Type::INTEGER:
+      {
+        if (a2.isNull() || a1.isNull())
+          return LVAL2(Value(Value::type_numeric), a1, a2);
+        Value val(Numeric(*a1.numeric() + *a2.integer()));
+        return LVAL2(val, a1, a2);
+      }
+      case Type::NUMERIC:
+      {
+        if (a2.isNull() || a1.isNull())
+          return LVAL2(Value(Value::type_numeric), a1, a2);
+        Value val(Numeric(*a1.numeric() + *a2.numeric()));
+        return LVAL2(val, a1, a2);
+      }
+      default:
+        break;
+      }
+      break;
+    case Type::LITERAL:
+      switch (a2.type().major())
+      {
+      case Type::NO_TYPE:
+        return a1;
+      case Type::LITERAL:
+      {
+        if (a1.isNull())
+          return a2;
+        if (a1.lvalue())
+        {
+          Value& val = ctx.allocate(a1.clone());
+          if (!a2.isNull())
+            val.literal()->append(*a2.literal());
+          return val;
+        }
+        if (!a2.isNull())
+          a1.literal()->append(*a2.literal());
+        return a1;
+      }
+      default:
+        break;
+      }
+      break;
+    default:
+      break;
     }
-    Value val(Integer(*a1.integer() + *a2.integer()));
-    return LVAL2(val, a1, a2);
-  }
-  case Type::NUMERIC:
-  {
-    if (a1.isNull())
-      return a1;
-    Value& a2 = arg2->value(ctx);
-    if (a2.isNull())
-      return a2;
-    if (a2.type() == Type::INTEGER)
-    {
-      Value val(Numeric(*a1.numeric() + *a2.integer()));
-      return LVAL2(val, a1, a2);
-    }
-    Value val(Numeric(*a1.numeric() + *a2.numeric()));
-    return LVAL2(val, a1, a2);
-  }
-  case Type::LITERAL:
-  {
-    Value& a2 = arg2->value(ctx);
-    if (a1.isNull())
-    {
-      if (a2.type() == Type::LITERAL)
-        return a2;
-      break; /* throw */
-    }
-    if (a1.lvalue())
-    {
-      Value& val = ctx.allocate(a1.clone());
-      if (!a2.isNull())
-        val.literal()->append(*a2.literal());
-      return val;
-    }
-    if (!a2.isNull())
-      a1.literal()->append(*a2.literal());
-    return a1;
-  }
-  default:
-    break;
   }
   throw RuntimeError(EXC_RT_INV_EXPRESSION);
 }
