fd0d5fc81c7bafa483cffc03c1c54bbef983d68a
janbar <jlbarriere68@gmail.com>
2025-03-20 14:43:23 +0100

    mysql: try to link with mariadb

--- modules/mysql/cmake/FindMySQL.cmake
+++ modules/mysql/cmake/FindMySQL.cmake
@@ -18,38 +18,19 @@
 SET( MYSQL_FOUND 0 )
 
 FIND_PATH(MYSQL_INCLUDE_DIRS mysql.h
-  /usr/include
-  /usr/include/mysql
-  /usr/local/include
-  /usr/local/include/mysql
-  /usr/local/mysql/include
-  /opt/local/include
-  /opt/local/include/mysql
-  /opt/local/mysql/include
+  PATH_SUFFIXES mysql mariadb/mysql
+  PATHS
   "C:/Program Files/MySQL/include"
-  "C:/Program Files/MySQL/MySQL Server 5.0/include"
   "C:/MySQL/include"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/include"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/include"
   DOC "Specify the directory containing mysql.h."
 )
 
 FIND_LIBRARY( MYSQL_LIBRARY
   NAMES mysql libmysql mysqlclient
+  PATH_SUFFIXES mysql mariadb/mysql
   PATHS
-  /usr/lib
-  /usr/lib/mysql
-  /usr/local/lib
-  /usr/local/lib/mysql
-  /usr/local/mysql/lib
-  /opt/local/lib
-  /opt/local/lib/mysql
-  /opt/local/mysql/lib
   "C:/Program Files/MySQL/lib"
-  "C:/Program Files/MySQL/MySQL Server 5.0/lib/opt"
-  "C:/MySQL/lib/debug"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
+  "C:/MySQL/lib"
   DOC "Specify the mysql library here."
 )
 
