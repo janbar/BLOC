35046c51728af6eb93fb6c8f94e177a682a681c7
janbar <jlbarriere68@gmail.com>
2025-10-25 14:14:12 +0200

    fix linking with readline on system SunOS

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -13,35 +13,40 @@
 #
 # Variables defined by this module:
 #
-#  READLINE_FOUND            System has readline, include and lib dirs found
-#  Readline_INCLUDE_DIR      The readline include directories. 
-#  Readline_LIBRARY          The readline library.
+#  Readline_FOUND            System has readline, include and lib dirs found
+#  Readline_INCLUDE_DIRS     The readline include directories. 
+#  Readline_LIBRARIES        The readline libraries.
 
-find_path(Readline_ROOT_DIR
-    NAMES include/readline/readline.h
-)
+if(NOT Readline_FOUND)
+    find_path(Readline_ROOT_DIR
+        NAMES include/readline/readline.h
+    )
 
-find_path(Readline_INCLUDE_DIR
-    NAMES readline/readline.h
-    HINTS ${Readline_ROOT_DIR}/include
-)
+    find_path(Readline_INCLUDE_DIR
+        NAMES readline/readline.h
+        HINTS ${Readline_ROOT_DIR}/include
+    )
 
-find_library(Readline_LIBRARY
-    NAMES readline
-    HINTS ${Readline_ROOT_DIR}/lib
-)
+    find_library(Readline_LIBRARY
+        NAMES readline
+        HINTS ${Readline_ROOT_DIR}/lib
+    )
 
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  FIND_LIBRARY(Readline_LIBRARY NAMES readline)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY )
-  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+    include(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY)
 
-mark_as_advanced(
-    Readline_ROOT_DIR
-    Readline_INCLUDE_DIR
-    Readline_LIBRARY
-)
+    if(Readline_FOUND)
+        set(Readline_LIBRARIES ${Readline_LIBRARY})
+        set(Readline_INCLUDE_DIRS ${Readline_INCLUDE_DIR})
+        if(${CMAKE_SYSTEM_NAME} STREQUAL "SunOS")
+            find_package(Curses QUIET)
+            if(Curses_FOUND)
+                list(APPEND Readline_LIBRARIES ${CURSES_LIBRARIES})
+            else()
+                set(Readline_FOUND FALSE)
+            endif()
+        endif()
+    endif()
+
+    MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
+endif()
